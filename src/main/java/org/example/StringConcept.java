190999872ce02a18619f243999098d7c2ec5d020
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-16 23:00:57 +0530

    Brush Ups

---
+++ src/main/java/org/example/StringConcept.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class StringConcept {
+
+    public static void main(String args[]){
+
+        System.out.println("String , StringBuilder & StringBuffer");
+
+        String s1 = "Hello";
+        String s2 = "Hello";
+        System.out.println(s1.equals(s2));
+
+        String a1 = new String("Sumit");
+        String a2 = new String("Sumit");
+
+        System.out.println(a1==a2);
+
+    }
+}
