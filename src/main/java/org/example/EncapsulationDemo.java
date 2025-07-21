4e592573366e195201047f1d21e9b3858561d31a
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-21 23:11:19 +0530

    OOPS

--- src/main/java/org/example/EncapsulationDemo.java
+++ src/main/java/org/example/EncapsulationDemo.java
@@ -4,7 +4,13 @@ class Fruits{
     private int weight;
     private String name;
 
-    
+    public void FruitName(String name){
+        System.out.println(name);
+    }
+
+    public int FruitWeight(int weight){
+        return weight;
+    }
 }
 
 
