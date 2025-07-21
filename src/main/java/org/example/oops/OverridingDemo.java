aeabb9ad0cd6cdc79146977b24e396d8ff9797ee
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-21 23:53:55 +0530

    Auto commit: 21/7/2025, 11:53:55 pm

--- src/main/java/org/example/oops/OverridingDemo.java
+++ src/main/java/org/example/oops/OverridingDemo.java
@@ -1,4 +1,17 @@
 package org.example.oops;
 
+
+// This is For Overriding
+
+class Shape{
+    void draw(){
+        System.out.println("Drawing Shape");
+    }
+}
+
 public class OverridingDemo {
+    public static void main(String args[]){
+
+
+    }
 }
