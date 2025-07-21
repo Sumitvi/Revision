fc38891cbe826b4c2bd6d65a11748dfb58fec1b1
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-21 23:45:45 +0530

    Auto commit: 21/7/2025, 11:45:45 pm

--- src/main/java/org/example/oops/OverLoadingDemo.java
+++ src/main/java/org/example/oops/OverLoadingDemo.java
@@ -1,11 +1,15 @@
 package org.example.oops;
 
 // Overloading means same method name but diffrent Arguements
+class Test{
+    
+}
+
 
 
 public class OverLoadingDemo {
 
     public static void main(String args[]){
-        
+
     }
 }
