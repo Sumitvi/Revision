c72d688e0642de4cb85a40a09fc680fae835b0b9
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-21 23:47:57 +0530

    Auto commit: 21/7/2025, 11:47:57 pm

--- src/main/java/org/example/oops/OverLoadingDemo.java
+++ src/main/java/org/example/oops/OverLoadingDemo.java
@@ -2,7 +2,19 @@ package org.example.oops;
 
 // Overloading means same method name but diffrent Arguements
 class Test{
-    
+    public int add(int a , int b){
+        return a+b;
+    }
+
+    public int add(int a , int b , int c){
+        return a+b+c;
+    }
+
+    public float add(int a , int b, int c , int d){
+
+        return a+b+c+d;
+
+    }
 }
 
 
@@ -10,6 +22,8 @@ class Test{
 public class OverLoadingDemo {
 
     public static void main(String args[]){
+
+        Test t1 = new Test();
 
     }
 }
