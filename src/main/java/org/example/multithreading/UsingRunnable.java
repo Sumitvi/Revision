2738c313fd6c4793e3d686c92e54c34a4fbbb4c0
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-24 22:58:40 +0530

    Auto commit: 24/7/2025, 10:58:40 pm

--- src/main/java/org/example/multithreading/UsingRunnable.java
+++ src/main/java/org/example/multithreading/UsingRunnable.java
@@ -5,7 +5,12 @@ class Test implements Runnable{
     @Override
     public void run(){
         for(int i=0;i<5;i++){
-            
+            try {
+                Thread.sleep(1000); // Sleep for 1 second
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+            System.out.println("Thread is running: " + i);
         }
     }
 }
