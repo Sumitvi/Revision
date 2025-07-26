70b71cd0e76e06e8379d6d27d28651685cfe4686
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-26 16:18:19 +0530

    Auto commit: 26/7/2025, 4:18:19 pm

--- src/main/java/org/example/multithreading/ThreadPriority.java
+++ src/main/java/org/example/multithreading/ThreadPriority.java
@@ -1,4 +1,18 @@
 package org.example.multithreading;
 
+class Demo implements Runnable{
+
+    @Override
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println()
+        }
+    }
+}
+
 public class ThreadPriority {
+
+    public static void main(String args[]){
+
+    }
 }
