e6b72003f0401e231c096a514f4df2a011e77710
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-26 16:18:52 +0530

    Auto commit: 26/7/2025, 4:18:52 pm

--- src/main/java/org/example/multithreading/ThreadPriority.java
+++ src/main/java/org/example/multithreading/ThreadPriority.java
@@ -5,7 +5,8 @@ class Demo implements Runnable{
     @Override
     public void run(){
         for(int i=0;i<5;i++){
-            System.out.println(Thread.currentThread())
+            System.out.println(Thread.currentThread());
+            
         }
     }
 }
