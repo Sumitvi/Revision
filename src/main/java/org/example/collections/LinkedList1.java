ab565395b36814e6838e3057e3cb83cbe0e096d3
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-28 08:30:31 +0530

    LinkedList Program

--- src/main/java/org/example/collections/LinkedList1.java
+++ src/main/java/org/example/collections/LinkedList1.java
@@ -1,4 +1,12 @@
 package org.example.collections;
+import java.util.*;
 
 public class LinkedList1 {
+
+    public static void main(String args[]){
+
+        LinkedList<String> list = new LinkedList<>();
+        
+
+    }
 }
