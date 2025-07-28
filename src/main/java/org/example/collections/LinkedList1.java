4e3c05a68a14929ced416630bd0550e0d498a412
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-28 08:40:50 +0530

    LinkedList-Collection

--- src/main/java/org/example/collections/LinkedList1.java
+++ src/main/java/org/example/collections/LinkedList1.java
@@ -6,7 +6,20 @@ public class LinkedList1 {
     public static void main(String args[]){
 
         LinkedList<String> list = new LinkedList<>();
-        
+        list.addFirst("Apple");
+        list.addLast("Orange");
+        System.out.println(list);
+
+        list.addFirst("Papaya");
+        list.addFirst("Banana");
+        list.addLast("Mango");
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+
+
+
 
     }
 }
