52703e23a69614453e843defc6da44858d9f2237
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-28 08:56:51 +0530

    LinkedList-Program For Practice

--- src/main/java/org/example/collections/LinkedListDemo.java
+++ src/main/java/org/example/collections/LinkedListDemo.java
@@ -1,4 +1,30 @@
 package org.example.collections;
 
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class LinkedListDemo {
+
+    public static void main(String args[]){
+
+        LinkedList<Integer> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+        int ele = sc.nextInt();
+
+        while(true){
+            System.out.println("Enter Elements to Add in First");
+            list.addFirst(ele);
+            System.out.println("New LinkedList is " + list);
+
+            System.out.println("Enter Elements to Add in Last");
+            list.addLast(ele);
+            System.out.println("New LinkedList is " + list);
+
+            if(list.size()>=4){
+                break;
+            }
+        }
+
+    }
 }
