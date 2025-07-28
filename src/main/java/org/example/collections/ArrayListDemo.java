882aebe488cf3df9e89d976c4146badd8d41fa0d
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-28 08:18:25 +0530

    ArrayList

---
+++ src/main/java/org/example/collections/ArrayListDemo.java
@@ -0,0 +1,44 @@
+package org.example.collections;
+import java.util.*;
+
+public class ArrayListDemo {
+
+    public static void main(String args[]){
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i=1;i<5;i++){
+            list.add(i);
+        }
+
+        System.out.println("List is " + list);
+
+       while(true){
+
+           System.out.println("Enter Element to Remove");
+           Scanner sc = new Scanner(System.in);
+           int index = sc.nextInt();
+
+           if(index<list.size()){
+               list.remove(index);
+               System.out.println("List is " + list);
+           }else{
+               System.out.println("No List Found");
+           }
+
+
+           System.out.println("Enter Element to Check");
+           int ele = sc.nextInt();
+           if(list.contains(ele)){
+               System.out.println("we have Element Exist");
+           }else{
+               System.out.println("We dont have ele Exists");
+           }
+
+           if(index == -1){
+               System.exit(0);
+           }
+       }
+
+       }
+}
