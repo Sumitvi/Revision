9290866de29af3c52673b5ad06b7bb0656348bdf
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-28 09:06:20 +0530

    LinkedList-Program For Practice

--- src/main/java/org/example/collections/LinkedListDemo.java
+++ src/main/java/org/example/collections/LinkedListDemo.java
@@ -10,21 +10,38 @@ public class LinkedListDemo {
 
         LinkedList<Integer> list = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
-        int ele = sc.nextInt();
+
+
 
         while(true){
             System.out.println("Enter Elements to Add in First");
+            int ele = sc.nextInt();
             list.addFirst(ele);
             System.out.println("New LinkedList is " + list);
 
             System.out.println("Enter Elements to Add in Last");
-            list.addLast(ele);
+            int ele1 = sc.nextInt();
+
+            list.addLast(ele1);
             System.out.println("New LinkedList is " + list);
 
             if(list.size()>=4){
                 break;
             }
         }
+
+        while(true){
+            System.out.println("Remove Element");
+            int removeele = sc.nextInt();
+            list.remove(removeele);
+            System.out.println("List is " + list);
+
+            if(list.size()>=4){
+                System.out.println("No Such List Exist");
+                break;
+            }
+        }
+
 
     }
 }
