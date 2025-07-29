1c132647a19542d6a8bd4ad335bf6caf3821f359
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-29 17:09:01 +0530

    MaxHeap

--- src/main/java/org/example/collections/MaxHeap.java
+++ src/main/java/org/example/collections/MaxHeap.java
@@ -19,5 +19,13 @@ public class MaxHeap {
 
         System.out.println(pq);
         System.out.println(pq.peek());
+
+//        iterating pq
+
+        System.out.println("Iterations ");
+//        if it's a max heap so it will iterate from the max element
+        for(int num : pq){
+            System.out.println(num);
+        }
     }
 }
