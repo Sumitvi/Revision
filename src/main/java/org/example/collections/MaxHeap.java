3fbb0bbe360fceff5299a721083379e6adfe505d
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-29 17:07:00 +0530

    MaxHeap

--- src/main/java/org/example/collections/MaxHeap.java
+++ src/main/java/org/example/collections/MaxHeap.java
@@ -1,4 +1,23 @@
 package org.example.collections;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class MaxHeap {
+
+    public static void main(String args[]){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        pq.add(10);
+        pq.add(20);
+        pq.add(30);
+        pq.add(40);
+
+        System.out.println(pq.peek());  // 40 - bcz its max heap
+        System.out.println(pq.poll());
+        System.out.println(pq.poll());
+
+        System.out.println(pq);
+        System.out.println(pq.peek());
+    }
 }
