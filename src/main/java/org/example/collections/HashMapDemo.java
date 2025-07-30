8fcdc0b97e9989765d11fbd5049469065d4f5f9b
Sumit Vishwakarma <sv0042595@gmail.com>
2025-07-30 11:29:44 +0530

    HashMap

--- src/main/java/org/example/collections/HashMapDemo.java
+++ src/main/java/org/example/collections/HashMapDemo.java
@@ -1,8 +1,39 @@
 package org.example.collections;
 
+import java.util.HashMap;
+
 public class HashMapDemo {
 
     public static void main(String args[]){
-        
+
+        HashMap<Integer , Integer> map = new HashMap<>();
+//        map.put(0, 1);
+//        map.put(1, 2);
+//        map.put(2, 4);
+//        map.put(3, 5);
+//        map.put(4, 6);
+//        map.put(5, 8);
+        int arr[] = {1 , 1 , 2 , 3 , 4, 3 ,4 ,3 ,4 ,1 ,1};
+
+        System.out.println(map);
+
+//        Caluculating Frequencies in hashmap
+
+        for(int num : arr){
+            map.put(num , map.getOrDefault(num , 0) + 1);
+        }
+
+        System.out.println(map);
+
+//        need to return the element whose frequency is 1
+
+        for(int i=0;i<map.size();i++){
+            if(map.containsKey()>1){
+                
+            }
+        }
+
+
+
     }
 }
